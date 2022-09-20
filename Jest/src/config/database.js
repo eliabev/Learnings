330e1e49acc3ffbccb5664557f474c06e082d8db
Eliabe-ciandt <eliabe@ciandt.com>
2022-09-20 17:33:41 -0300

    Beggins Jest project

---
+++ Jest/src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'eliabe',
+  password: '123456789',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: '0',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
